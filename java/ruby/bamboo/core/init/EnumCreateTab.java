74582a7178d043681482ed18ac90b482b4182fb2
rubnsn <rubnsn@users.noreply.github.com>
2016-11-26 13:41:30 +0900

    クリエイティブのアイコン指定の修正。 なんでmap保持してたのか忘れたから撤去

--- java/ruby/bamboo/core/init/EnumCreateTab.java
+++ java/ruby/bamboo/core/init/EnumCreateTab.java
@@ -1,30 +1,25 @@
 package ruby.bamboo.core.init;
 
-import java.util.HashMap;
-
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import ruby.bamboo.api.BambooBlocks;
 import ruby.bamboo.api.Constants;
 
 public enum EnumCreateTab {
-    NONE,
-    TAB_BAMBOO;
-    static HashMap<EnumCreateTab, CreativeTabs> map;
+    NONE(null),
+    TAB_BAMBOO( new CreativeTabs(Constants.MODID) {
 
-    static {
-        map = new HashMap<EnumCreateTab, CreativeTabs>();
-        map.put(NONE, null);
-        map.put(TAB_BAMBOO, new CreativeTabs(Constants.MODID) {
+        @Override
+        public Item getTabIconItem() {
+            return Item.getItemFromBlock(BambooBlocks.BAMBOOSHOOT);//Block.getBlockFromName(BlockData.getModdedName(ItemBambooShoot.class)));
+        }
+    });
 
-            @Override
-            public Item getTabIconItem() {
-                return Item.getItemFromBlock(Blocks.STONE);//Block.getBlockFromName(BlockData.getModdedName(ItemBambooShoot.class)));
-            }
-        });
+    EnumCreateTab(CreativeTabs tab){
+        this.tab=tab;
     }
-
+    CreativeTabs tab;
     public CreativeTabs getTabInstance() {
-        return map.get(this);
+        return tab;
     }
 };
